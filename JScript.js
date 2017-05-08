60128e65350efb5a830fe1d5a8843671c570eacd
whatsmy2cents <whats_my_2_cents@yahoo.com>
2017-05-08 14:02:27 -0400

    Updated jquery and HTML to include a shade and clear button.  Updated css to better size container

--- JScript.js
+++ JScript.js
@@ -18,7 +18,9 @@ $(document).ready( function() {
 		$('td').hover(function() {
 			$(this).css("background-color","Black");
 		});
+		
 	$('#gridBtn').click( function () {
+		$('td').off();
 		var usrInput = prompt("Enter squar grid size!");
 		$('#screen').empty();
 		$('#screen').append('<table id="grid"></table>')
@@ -31,7 +33,7 @@ $(document).ready( function() {
 			$('#grid').append('<tr id="'+ row + '">');
 			for(col = 0; col <= (usrInput - 1); col++)
 			{
-				$('#' + row + '').append('<td class="pix" style="height: ' + height + '; width:' + width +';"></td>');
+				$('#' + row + '').append('<td class="pix" style="opacity:0.1; height: ' + height + '; width:' + width +';"></td>');
 			}
 			$('#grid').append('</tr>');
 		}
@@ -40,8 +42,12 @@ $(document).ready( function() {
 			$(this).css("background-color","Black");
 		});
 	});
+
+	
 	$('#RainbowBtn').click( function () {
+		$('td').off();
 		$('td').hover(function() {
+			$(this).css("opacity",1);
 			var red = Math.floor((Math.random() * 256));
 			var green = Math.floor((Math.random() * 256));
 			var blue = Math.floor((Math.random() * 256));
@@ -49,15 +55,41 @@ $(document).ready( function() {
 			$(this).css("background-color", "rgb(" + red + "," + green + "," + blue + ")");
 		});
 	});
-
-	$('#permBtn').click( function () {
-				$('td').hover(function() {
-			$(this).css("background-color","Black");
+	
+	$('#shadeBtn').click( function () {
+		$('td').off();
+		$('td').hover(function() {
+			if(parseFloat($(this).css("opacity")) < 1)
+			{
+				var opacity = parseFloat($(this).css("opacity"));
+				opacity += 0.10;
+				
+				$(this).css("opacity",opacity);
+			}
+			else
+			{
+				$(this).css("opacity",0.1);
+			}	
 		});
 	});
+
+	$('#permBtn').click( function () {
+		$('td').off();
+		$('td').hover(function() {
+			$(this).css("opacity",1);
+			$(this).css("background-color","rgba(0,0,0,1)");
+		});
+	});
+	
 	$('#EraserBtn').click( function () {
-				$('td').hover(function() {
+		$('td').off();
+		$('td').hover(function() {
 			$(this).css("background-color","White");
 		});
+	});
+	$('#clrBtn').click( function () {
+		$('td').off();
+		$('td').css("background-color","White");
+		$(this).css("background-color","rgba(0,0,0)");
 	});
 });
